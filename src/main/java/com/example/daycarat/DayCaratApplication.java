72763be9b17de5c80a0e6876a95792ab14260e1c
Seo Chanhyeok <popcorn@g.hongik.ac.kr>
2024-01-08 10:41:18 +0900

    fix: swagger server url

--- src/main/java/com/example/daycarat/DayCaratApplication.java
+++ src/main/java/com/example/daycarat/DayCaratApplication.java
@@ -1,9 +1,17 @@
 package com.example.daycarat;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@OpenAPIDefinition(
+		servers = {
+				@Server(url = "https://www.daycarat.store/api", description = "Production Server URL"),
+				@Server(url = "http://localhost:8080/api", description = "Dev Server URL")
+		}
+)
 @SpringBootApplication
 @EnableJpaAuditing
 public class DayCaratApplication {
