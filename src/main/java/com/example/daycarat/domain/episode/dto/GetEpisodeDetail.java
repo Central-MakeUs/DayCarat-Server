4d95492a7eb73cd8eeb4f7d1584b85236039b83e
Seo Chanhyeok <popcorn@g.hongik.ac.kr>
2024-01-16 15:29:41 +0900

    doc: swagger description

--- src/main/java/com/example/daycarat/domain/episode/dto/GetEpisodeDetail.java
+++ src/main/java/com/example/daycarat/domain/episode/dto/GetEpisodeDetail.java
@@ -8,7 +8,7 @@ public record GetEpisodeDetail(
         @Schema(description = "에피소드 ID", example = "1") Long episodeId,
         @Schema(description = "제목", example = "팀플 회의") String title,
         @Schema(description = "활동태그명", example = "CMC") String activityTagName,
-        @Schema(description = "선택 날짜", example = "2024-01-20") String selectedDate,
+        @Schema(description = "선택 날짜", example = "2024년 01월 09일 화요일") String selectedDate,
         List<GetEpisodeContent> episodeContents
 ) {
     public static GetEpisodeDetail of(Long episodeId, String title, String activityTagName, String selectedDate, List<GetEpisodeContent> episodeContents) {
