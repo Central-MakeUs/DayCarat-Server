99463747683d839028432a4028993e58f7073c76
Seo Chanhyeok <popcorn@g.hongik.ac.kr>
2024-01-20 18:05:29 +0900

    hotfix: fix data type

--- src/main/java/com/example/daycarat/domain/gereratedcontent/dto/PostGeneratedContent.java
+++ src/main/java/com/example/daycarat/domain/gereratedcontent/dto/PostGeneratedContent.java
@@ -2,7 +2,7 @@ package com.example.daycarat.domain.gereratedcontent.dto;
 
 public record PostGeneratedContent(
         Long episodeId,
-        int keywordId,
+        Integer keywordId,
         String s3ObjectKey,
         String generatedContent1,
         String generatedContent2,
