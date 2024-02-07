36f65e26fa799371159188e905cc52906dd3a11b
Seo Chanhyeok <popcorn@g.hongik.ac.kr>
2024-02-07 11:37:03 +0900

    feature: validation for id_token

--- src/main/java/com/example/daycarat/global/oauth/AppleUserService.java
+++ src/main/java/com/example/daycarat/global/oauth/AppleUserService.java
@@ -1,5 +1,6 @@
 package com.example.daycarat.global.oauth;
 
+import com.example.daycarat.domain.user.dto.ApplePublicKeyResponse;
 import com.example.daycarat.domain.user.dto.AppleUserDto;
 import com.example.daycarat.domain.user.entity.Role;
 import com.example.daycarat.domain.user.entity.User;
@@ -7,26 +8,28 @@ import com.example.daycarat.domain.user.repository.UserRepository;
 import com.example.daycarat.global.error.exception.CustomException;
 import com.example.daycarat.global.jwt.SecurityService;
 import com.example.daycarat.global.jwt.TokenResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.tuple.Pair;
-import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
-import org.bouncycastle.openssl.PEMParser;
-import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
-import java.io.Reader;
-import java.io.StringReader;
-import java.security.PrivateKey;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.KeyFactory;
+import java.security.PublicKey;
+import java.security.spec.RSAPublicKeySpec;
 import java.text.ParseException;
-import java.util.Date;
+import java.util.Base64;
+import java.util.Map;
 import java.util.UUID;
 
 import static com.example.daycarat.global.error.exception.ErrorCode.APPLE_LOGIN_FAILED;
@@ -51,6 +54,8 @@ public class AppleUserService {
 
 
     public Pair<TokenResponse, Boolean> appleLogin(String id_token) {
+        validate(id_token);
+
         AppleUserDto appleUserInfo = getUserInfo(id_token);
 
         Pair<User, Boolean> appleUser = registerAppleUserIfNeed(appleUserInfo);
@@ -66,7 +71,7 @@ public class AppleUserService {
             JWTClaimsSet claimsSet;
             try {
                 SignedJWT signedJWT = SignedJWT.parse(idToken);
-                claimsSet = (JWTClaimsSet) signedJWT.getJWTClaimsSet();
+                claimsSet = signedJWT.getJWTClaimsSet();
             } catch (ParseException e) {
                 throw new RuntimeException("Failed to decode id_token", e);
             }
@@ -108,38 +113,50 @@ public class AppleUserService {
         return Pair.of(appleUser, false);
     }
 
-    private String createClientSecret() {
-        Date now = new Date();
-        Date exp = new Date(now.getTime() + (1000 * 60 * 60 * 24 * 180)); // 180일 후 만료
+    private ApplePublicKeyResponse getApplePublicKey() {
+        String url = "https://appleid.apple.com/auth/keys";
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        ApplePublicKeyResponse applePublicKeyResponse;
 
         try {
-            return Jwts.builder()
-                    .setHeaderParam("kid", keyId)
-                    .setHeaderParam("alg", "ES256")
-                    .setIssuer(teamId)
-                    .setIssuedAt(new Date(System.currentTimeMillis()))
-                    .setExpiration(exp)
-                    .setAudience("https://appleid.apple.com")
-                    .setSubject(clientId)
-                    .signWith(getApplePrivateKey(), SignatureAlgorithm.ES256)
-                    .compact();
+            applePublicKeyResponse = objectMapper.readValue(response.getBody(), ApplePublicKeyResponse.class);
         } catch (Exception e) {
-            throw new RuntimeException("Failed to create client secret", e);
+            throw new CustomException(APPLE_LOGIN_FAILED);
         }
+
+        return applePublicKeyResponse;
     }
 
-    private PrivateKey getApplePrivateKey() {
+    private void validate(String identityToken) {
         try {
-            ClassPathResource resource = new ClassPathResource(keyPath);
-            String privateKey = new String(resource.getInputStream().readAllBytes());
-            Reader pemReader = new StringReader(privateKey);
-            PEMParser pemParser = new PEMParser(pemReader);
-            JcaPEMKeyConverter converter = new JcaPEMKeyConverter();
-            PrivateKeyInfo object = (PrivateKeyInfo)pemParser.readObject();
+            ApplePublicKeyResponse response = getApplePublicKey();
 
-            return converter.getPrivateKey(object);
+            String headerOfIdentityToken = identityToken.substring(0, identityToken.indexOf("."));
+            Map header = new ObjectMapper().readValue(new String(Base64.getDecoder().decode(headerOfIdentityToken), StandardCharsets.UTF_8), Map.class);
+            ApplePublicKeyResponse.Key key = response.getMatchedKeyBy((String) header.get("kid"), (String) header.get("alg"))
+                    .orElseThrow(() -> new NullPointerException("Failed get public key from apple's id server."));
+
+            byte[] nBytes = Base64.getUrlDecoder().decode(key.getN());
+            byte[] eBytes = Base64.getUrlDecoder().decode(key.getE());
+
+            BigInteger n = new BigInteger(1, nBytes);
+            BigInteger e = new BigInteger(1, eBytes);
+
+            RSAPublicKeySpec publicKeySpec = new RSAPublicKeySpec(n, e);
+            KeyFactory keyFactory = KeyFactory.getInstance(key.getKty());
+            PublicKey publicKey = keyFactory.generatePublic(publicKeySpec);
+
+            Claims body = Jwts.parser().setSigningKey(publicKey).parseClaimsJws(identityToken).getBody();
+
+            if (!body.getIssuer().equals("https://appleid.apple.com") || !body.getAudience().equals(clientId)) {
+                throw new CustomException(APPLE_LOGIN_FAILED);
+            }
+
         } catch (Exception e) {
-            throw new RuntimeException("Failed to get apple private key", e);
+            throw new CustomException(APPLE_LOGIN_FAILED);
         }
     }
 
